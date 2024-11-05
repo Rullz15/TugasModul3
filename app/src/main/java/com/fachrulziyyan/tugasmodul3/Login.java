df0c78976f9af4365ff55eceeb7bfb84ba49ffbd
Fachrul <holla.rullz@gmail.com>
2024-11-05 18:37:50 +0700

    Initial commit

---
+++ app/src/main/java/com/fachrulziyyan/tugasmodul3/Login.java
@@ -0,0 +1,29 @@
+package com.fachrulziyyan.tugasmodul3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = findViewById(R.id.button3);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Dashboard.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/fachrulziyyan/tugasmodul3/MainActivity.java
@@ -0,0 +1,36 @@
+package com.fachrulziyyan.tugasmodul3;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.button_login);
+        Button registerButton = findViewById(R.id.button_register);
+
+        // Intent untuk login
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        // Intent untuk register
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
